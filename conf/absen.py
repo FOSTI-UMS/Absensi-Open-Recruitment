ac587d4f51b9ac3c8871ce371558240bf070c25a
nchudlori <imlori000@gmail.com>
2018-08-10 21:25:28 +0700

    adding security and time

--- conf/absen.py
+++ conf/absen.py
@@ -11,10 +11,16 @@ try:
 	import zbar
 	import csv
 	import shutil
-	# import pwn
 except ImportError:
 	# print "Install module dulu, buka requirement.txt"
 	os.system("bash req.sh")
+	try:
+		from subprocess import call
+		import zbar
+		import csv
+		import shutil
+	except ImportError:
+		exit()
 
 class color:
    PURPLE = '\033[95m'
@@ -28,9 +34,9 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
-print color.BOLD + color.UNDERLINE + color.GREEN + "\t\tAbsensi Open Requirement FOSTI UMS" + color.END
-print color.RED + "\t\t\tCopyright : FOSTI UMS" + color.END
-print "\n"
+# print color.BOLD + color.UNDERLINE + color.GREEN + "\t\tAbsensi Open Requirement FOSTI UMS" + color.END
+# print color.RED + "\t\t\tCopyright : FOSTI UMS" + color.END
+# print "\n"
 
 data = open("Oprec.csv", "a")
 
@@ -56,24 +62,37 @@ proc.visible = False
 nomer = 0
 # Font coloring
 
-fontcolor = [color.PURPLE,color.CYAN,color.DARKCYAN,color.BLUE, color.GREEN, color.YELLOW, color.RED]
+fontcolor = [color.PURPLE,color.CYAN,color.DARKCYAN,color.BLUE, color.GREEN, color.YELLOW]
 
 for symbol in proc.results:
     # do something useful with results
     # print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
-	nomer+=1
+	# nomer+=1
 	qrsp = symbol.data.split('/')
-	nama = qrsp[1]
-	nim = qrsp[0]
-	data.write (str(nomer))
-	data.write (",")
+	if len(qrsp) != 4:
+		print color.BOLD + color.RED + "Invalid QR length ............................[Error]" + color.END
+		break
+	nama = qrsp[2]
+	nim = qrsp[1]
+	unique_id = qrsp[3]
+	valid = qrsp[0]
+	waktu = time.strftime('Pukul %H.%M.%S WIB')
+	# data.write (str(nomer))
+	# data.write (",")
+	if valid != "Fosti_Oprec":
+		print color.BOLD + color.RED + "Invalid QR ............................[Error]"
+		break
 	data.write (nama)
 	data.write (",")
 	data.write (nim)
+	data.write (",")
+	data.write (unique_id)
+	data.write (",")
+	data.write (waktu)
 	data.write (",")
 	data.write ("\n")
 	data.close()
 
 	# print "Selamat datang "+nama
-	print color.BOLD + random.choice(fontcolor)+"Selamat datang "+nama
+	print color.BOLD + random.choice(fontcolor)+"Selamat datang "+ nama + "..........................[OK]"
 	print
--- run.py
+++ run.py
@@ -5,17 +5,45 @@ import os
 try:
 	import Tkinter
 	import tkMessageBox
+	from subprocess import call
+	import zbar
 	import csv
 	import shutil
 	# import pwn
 except ImportError:
 	# print "Install module dulu, buka requirement.txt\n"
-	os.system("bash req.sh")
+	os.system("bash conf/req.sh")
+	try:
+		import Tkinter
+		import tkMessageBox
+		from subprocess import call
+		import zbar
+		import csv
+		import shutil
+		# import pwn
+	except ImportError:
+		exit()
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   
+print color.BOLD + color.UNDERLINE + color.GREEN + "\n\t\tAbsensi Open Requirement FOSTI UMS" + color.END
+print color.RED + "\t\t\tCopyright : FOSTI UMS" + color.END
+print "\n"
 
 root = Tkinter.Tk()
 
 file = open("Oprec.csv", "w")
-file.write("No,Nama,NIM\n")
+file.write("Nama,NIM,ID,Waktu\n")
 file.close()
 
 # x = open("Oprec.csv")
@@ -27,7 +55,7 @@ file.close()
 root.title("Absensi Oprec")
 
 def absenCallBack():
-   os.system("python absen.py")
+   os.system("python conf/absen.py")
 
 B = Tkinter.Button(root, text ="Absen", command = absenCallBack, height = 10, width = 30)
 def on_closing():
@@ -35,7 +63,7 @@ def on_closing():
 		x=str( time.strftime('%A. %d %B %Y - Pukul %H.%M WIB'))
 		os.makedirs( r'ABSEN OPEN RECUIREMENT - %s'%x)
 		shutil.move('Oprec.csv', 'ABSEN OPEN RECUIREMENT - %s'%x)
-		print "Terima kasih!!"
+		print color.BOLD + color.GREEN + "\t\t\tTerima kasih!!"
 		root.destroy()
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
