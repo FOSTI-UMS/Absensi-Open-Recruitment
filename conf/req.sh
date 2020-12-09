d70670636ef20067578de8fafa6319f77419a411
alfian kan <alfiankan@alfians-MacBook-Pro.local>
2020-12-09 10:55:08 +0700

    add new dependencies

--- conf/req.sh
+++ conf/req.sh
@@ -16,8 +16,9 @@ echo -e $red "SABAR!!!"
 echo -e $white
 sleep 2
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+  pip3 install zbar-py
+  pip3 install opencv-python
 	sudo apt-get install zbar-tools -y
-	sudo apt-get install python-zbar -y
 	sudo apt-get install python-tk -y
   echo -e $grn "Proses instalasi selesai!"
   echo -e $white
